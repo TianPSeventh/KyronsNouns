6727c10b8ebef950b75c93eb6167fa69c4d3e8a0
Tian <cqpanuncillo@gmail.com>
2021-04-04 07:02:19 +0800

    Beta

--- scripts/global/data.gd
+++ scripts/global/data.gd
@@ -1,5 +1,7 @@
-extends PopupPanel
+extends Control
 #Contains all the variabled data
+
+const WINDOW_SIZE := OS.window_size
 
 var Options = null
 var Data = null
@@ -7,11 +9,22 @@ var BGME = "BGME"
 var LandscapeMode:bool = false
 var ListOfNodesAffectedByOrientation: Array = []
 
+
+
+
+#func _ready():
+#	get_viewport().connect("size_changed", self, "_on_size_changed")
+
+#func _on_size_changed():
+#	var s := OS.window_size
+#
+#	#emit_signal("screen_orientation_changed",  "landscape" if WINDOW_SIZE.x < s.x else "portrait")
+#	return   # somehow function triggers twice
+
 func openOptions(OpenOption:bool = true):
-	if OpenOption:
-		popup_centered()
-	else:
-		hide()
+	if get_index() != get_parent().get_child_count() - 1:
+		get_parent().move_child(self, get_parent().get_child_count() - 1)
+	funcref(self,"show" if OpenOption else "hide").call_func()
 
 func toggleOptions():
 	openOptions(!visible)
@@ -37,7 +50,12 @@ func _on_Main_pressed():
 	GM.changeScene(self,"res://scene/main.tscn")
 
 
-func _on_Toggle_toggled(button_pressed):
+func OrientationLandscape(button_pressed):
 	LandscapeMode = button_pressed
+	
 	for x in ListOfNodesAffectedByOrientation:
 		x.changeOrientation(button_pressed)
+
+
+func _on_BGME_finished():
+	pass # Replace with function body.
